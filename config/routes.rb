5a068a943184c0536088bf8bca007bf5efc2134c
Peter Giacomo Lombardo <pglombardo@hey.com>
2022-03-20 21:54:19 +0100

    Add ability to run app in subfolder

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,47 @@
 Rails.application.routes.draw do
-  localized do
-    devise_for :users, skip: :registrations, controllers: {
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-      unlocks: 'users/unlocks',
-      confirmations: 'users/confirmations'
-    }
+  routes_config = Proc.new {
+    localized do
+      devise_for :users, skip: :registrations, controllers: {
+        sessions: 'users/sessions',
+        passwords: 'users/passwords',
+        unlocks: 'users/unlocks',
+        confirmations: 'users/confirmations'
+      }
 
-    devise_scope :user do
-      resource  :registration,
-                only: %i[new create edit update],
-                path: 'users',
-                path_names: { new: 'sign_up' },
-                controller: 'users/registrations',
-                as: :user_registration do
-                  get :cancel
-                end
+      devise_scope :user do
+        resource  :registration,
+                  only: %i[new create edit update],
+                  path: 'users',
+                  path_names: { new: 'sign_up' },
+                  controller: 'users/registrations',
+                  as: :user_registration do
+                    get :cancel
+                  end
+      end
+
+      get '/d/active' => 'dashboard#active', as: :dashboard_active
+      get '/d/expired' => 'dashboard#expired', as: :dashboard_expired
+
+      resources :p, controller: :passwords, as: :passwords, except: %i[index edit update] do
+        get 'preview', on: :member
+        get 'r', on: :member, as: 'preliminary', action: 'preliminary'
+        get 'audit', on: :member
+      end
+
+      resources :c, controller: :commands, as: :commands, allow: %i[create]
+      get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
+      get '/pages/*id' => 'pages#show', as: :page, format: false
+      resources :feedbacks, only: %i[new create]
+      root to: 'passwords#new', :locale => I18n.default_locale
     end
+  }
 
-    get '/d/active' => 'dashboard#active', as: :dashboard_active
-    get '/d/expired' => 'dashboard#expired', as: :dashboard_expired
-
-    resources :p, controller: :passwords, as: :passwords, except: %i[index edit update] do
-      get 'preview', on: :member
-      get 'r', on: :member, as: 'preliminary', action: 'preliminary'
-      get 'audit', on: :member
+  # This allows for running the application in a subfolder.  See config/settings.yml (relative_root)
+  if Settings.relative_root
+    scope(:path => Settings.relative_root) do
+      routes_config.call
     end
-
-    resources :c, controller: :commands, as: :commands, allow: %i[create]
-    get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
-    get '/pages/*id' => 'pages#show', as: :page, format: false
-    resources :feedbacks, only: %i[new create]
-    root to: 'passwords#new', :locale => I18n.default_locale
+  else
+    routes_config.call
   end
 end
